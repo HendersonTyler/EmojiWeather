9d4810845acecf79867bc3db94036f1ad955d3f3
HendersonTyler <henderson.tyler@outlook.com>
2020-07-23 14:14:31 +1000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reploading.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="App container">
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" className="d-inline-block align-top" alt="" loading="lazy" />
+            ⛈ Emoji Forecast    
         </a>
-      </header>
-    </div>
+      </nav>
+      <div className="text-center pt-2"><h2>24 Australian Forecast</h2></div>
+      
+
+        <div className="pt-5">
+          <p>🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊</p>
+          <p>🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊☀🌊🌊🌊🌊🌊🌊</p>
+          <p>🌊🌊🌊🌊🌊🌊☀☀🌊🌊🌊🌊🌊☀☀🌊🌊🌊🌊🌊</p>
+          <p>🌊🌊🌊🌊☀☀☀☀☀☀☀🌊🌊☀☀☀🌊🌊🌊🌊</p>
+          <p>🌊🌊☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀🌊🌊🌊</p>
+          <p>🌊☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀🌊🌊</p>
+          <p>🌊☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀🌊</p>
+          <p>🌊☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀🌊🌊</p>
+          <p>🌊🌊☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀🌊🌊🌊</p>
+          <p>🌊🌊🌊🌊☀☀☀🌊☀☀☀☀☀☀☀☀🌊🌊🌊🌊</p>
+          <p>🌊🌊🌊🌊🌊☀🌊🌊🌊🌊🌊🌊☀☀☀🌊🌊🌊🌊🌊</p>
+          <p>🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊</p>
+          <p>🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊☀🌊🌊🌊🌊🌊🌊</p>
+          <p>🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊</p>
+        </div>
+      </div>
+   
   );
 }
 
