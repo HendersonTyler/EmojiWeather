e6c9b5c89554e6b5e9c558db7b075a1e5551828b
HendersonTyler <henderson.tyler@outlook.com>
2020-08-04 15:06:47 +1000

    Added Background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CLOUDS from 'vanta/dist/vanta.fog.min';
+import * as THREE from 'three';
 
 
 
-function App() {
+
+const App = () => {
 
   const [forecast, setForecast] = useState([]);
-
+  const [vantaEffect, setVantaEffect] = useState(0)
+  const myRef = useRef(null)
+  
   useEffect(() => {
     const getForecast = async () => {
       const response = await fetch("http://127.0.0.1:8000/forecast");
@@ -18,55 +23,60 @@ function App() {
         }
     };
     getForecast();
-  }, [])
+
+    if (!vantaEffect) {
+      setVantaEffect(CLOUDS({
+        el: myRef.current,mouseControls: true,
+        mouseControls: true,
+        touchControls: true,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        highlightColor: 0xffffff,
+        midtoneColor: 0xc8c59d,
+        lowlightColor: 0xffffff,
+        baseColor: 0x6396f5,
+        THREE: THREE,
+      }))
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy()
+    }
+
+    
+  }, [vantaEffect])
 
   // const forecast = [{"_id":"5f20b82d1e62111674862b55","location":0,"place":"","result":"w"},{"_id":"5f20b8596fce7588f46c8a80","location":1,"place":"","result":"w"},{"_id":"5f20b8826fce7588f46c8a81","location":2,"place":"","result":"w"},{"_id":"5f20b89e6fce7588f46c8a82","location":3,"place":"QCL091","result":"s"},{"_id":"5f20b8c66fce7588f46c8a83","location":4,"place":"","result":"w"}];
 
   return (
-    <div className="App container">
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" className="d-inline-block align-top" alt="" loading="lazy" />
-            ⛈ Emoji Forecast    
-        </a>
-      </nav>
-      <div className="text-center pt-2"><h2>Tomorrow Forecast</h2></div>
-      
-      {forecast ? (
-        <div>
-          {forecast.map((value) => {
-            if (value.result === 'b'){
-              return <p></p>
-            } else {
-              return value.result
-            } 
-            
-          })}
-        </div>
-      ) : (
-        <div>
-          <p>Loading...</p>
-        </div>
-      )}
+    <div ref={myRef}>
+      <div className="container vh-100">
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" className="d-inline-block align-top" alt="" loading="lazy" />
+              ⛈ Emoji Forecast    
+          </a>
+        </nav>
+        <div className="text-center pt-2"><h2>Australian Forecast</h2></div>
         
-        {/* <div className="pt-5">
-          <p>🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊</p>
-          <p>🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊☀🌊🌊🌊🌊🌊🌊</p>
-          <p>🌊🌊🌊🌊🌊🌊☀☀🌊🌊🌊🌊🌊☀☀🌊🌊🌊🌊🌊</p>
-          <p>🌊🌊🌊🌊☀☀☀☀☀☀☀🌊🌊☀☀☀🌊🌊🌊🌊</p>
-          <p>🌊🌊☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀🌊🌊🌊</p>
-          <p>🌊☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀🌊🌊</p>
-          <p>🌊☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀🌊</p>
-          <p>🌊☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀🌊🌊</p>
-          <p>🌊🌊☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀🌊🌊🌊</p>
-          <p>🌊🌊🌊🌊☀☀☀🌊☀☀☀☀☀☀☀☀🌊🌊🌊🌊</p>
-          <p>🌊🌊🌊🌊🌊☀🌊🌊🌊🌊🌊🌊☀☀☀🌊🌊🌊🌊🌊</p>
-          <p>🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊</p>
-          <p>🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊☀🌊🌊🌊🌊🌊🌊</p>
-          <p>🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊</p>
-        </div> */}
-      </div>
-   
+        {forecast ? (
+          <div className="card text-center">
+            {forecast.map((value) => {
+              if (value.result === 'b'){
+                return <p></p>
+              } else {
+                return value.result
+              } 
+              
+            })}
+          </div>
+        ) : (
+          <div>
+            <p>Loading...</p>
+          </div>
+        )}
+          
+        </div>
+      </div>   
   );
 }
 
