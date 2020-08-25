6738499f93ff87ec8f601d569d6bf5c55f84d06b
HendersonTyler <henderson.tyler@outlook.com>
2020-08-25 15:48:40 +1000

    Updated Address

--- src/location.js
+++ src/location.js
@@ -11,7 +11,7 @@ const Location = props => {
     const [ready, setReady] = useState(false);
     
     useEffect(() => {
-    fetch("http://localhost:8000/town/" + city )
+    fetch("http://sweet-source.bnr.la:8000/town/" + city )
     .then(response => {
         if (!response.ok) {
             throw new Error('HTTP error ' + response.status);
